13163bdd2510bc054a78fd7c78509b6ce6d9d814
clecle253 <clement.cazaubon@epitech.eu>
2018-05-26 15:26:55 +0200

    adding beining of struct client

--- include/client.h
+++ include/client.h
@@ -5,8 +5,15 @@
 **
 */
 
-struct client_data
+typedef struct s_client_epoll
 {
-        int epoll_fd
-        char[10]
-};
+	int epollfd;
+	struct epoll_event event;
+	struct epoll_event events[10];
+} t_client_epoll;
+
+typedef struct s_client_data
+{
+	t_client_epoll *client;
+        char name[10];
+} t_client_data;
