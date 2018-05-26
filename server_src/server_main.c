a9d603770d0eed9e15a6eea5e4f12c9083907390
Trevis Gulby <trevisgulby@protonmail.ch>
2018-05-26 12:50:02 +0200

    split functions to other files

--- server_src/server_main.c
+++ server_src/server_main.c
@@ -11,50 +11,6 @@
 #include <sys/wait.h>
 #include <sys/prctl.h>
 #include <arpa/inet.h>
-#include <regex.h>
-
-/** Dummy signal hander to exit server properly on console with CTRL-C
-* @param signo the catched signal type (hit ```man 2 signal```)
-*/
-void sig_handler(int signo)
-{
-	if (signo == SIGINT) {
-		printf("\nReceived SIGINT\n\tSee u !..\n");
-		exit(0);
-	}
-}
-
-/** Match all methods in G_CMDS for a given req command string
-* @param req the requested command on format 'CMD <space> [ARGS]'
-* @param clifd the client socket fd
-*/
-static int	get_methods(char *req, int clifd)
-{
-	int index = 0;
-	int i = 0;
-	int j = 0;
-	char *str, *arg = NULL;
-	char args[MAXARGS][MAXARGSIZE] = {{0},{0}};
-
-	str = strdup(req);
-	arg = strtok(str, " \r\n");
-	while (arg) {
-		strncpy(args[i++], arg, MAXARGSIZE);
-		arg = strtok(NULL, " \r\n");
-	}
-	print_arg(args);
-	for (j = 0; args[j]; ++j) {
-		for (index = 0; G_PROTOS[index]; ++index) {
-			if ((strcasestr(args[j], G_PROTOS[index]))) {
-				index = G_CMDS[index](args, clifd);
-				break;
-			}
-		}
-	}
-
-	memset(req, 0, BUF_SIZE);
-	return ((index == REF_NB) ? (0) : (index));
-}
 
 /** Set the new accept() client socket in epoll event list
 * for new client handling and write 220 + [server header] on client socket
